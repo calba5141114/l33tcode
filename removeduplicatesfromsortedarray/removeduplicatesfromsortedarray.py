28a0c963f9cf7335ee2f50e21f03b7a13086cf54
Carlos Alba <soradev4@gmail.com>
2019-06-13 22:36:23 -0700

    Update removeduplicatesfromsortedarray.py

--- removeduplicatesfromsortedarray/removeduplicatesfromsortedarray.py
+++ removeduplicatesfromsortedarray/removeduplicatesfromsortedarray.py
@@ -6,5 +6,11 @@ class Solution:
                 nums[next_new] = nums[i]
                 next_new += 1
         return next_new
+    
+    
+    '''
+    Runtime: 64 ms, faster than 56.42% of Python3 online submissions for Remove Duplicates from Sorted Array.
+    Memory Usage: 15 MB, less than 13.99% of Python3 online submissions for Remove Duplicates from Sorted Array.
+    '''
         
         
