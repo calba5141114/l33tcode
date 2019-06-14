c89758ad4a3acb8acde064048ebe20aed3a821a9
Carlos Alba <soradev4@gmail.com>
2019-06-13 22:49:21 -0700

    Update removeelement.py

--- removeelement/removeelement.py
+++ removeelement/removeelement.py
@@ -6,3 +6,11 @@ class Solution:
                 nums[next_free] = num
                 next_free += 1
         return next_free
+    
+'''
+Runtime: 36 ms, faster than 86.62% of Python3 online submissions for Remove Element.
+Memory Usage: 13.2 MB, less than 26.22% of Python3 online submissions for Remove Element.
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+'''
