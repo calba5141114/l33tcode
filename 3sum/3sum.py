ef7e5b070e854380498b28969c8c4a2c25efe149
Carlos Alba <soradev4@gmail.com>
2019-06-13 21:29:24 -0700

    Update 3sum.py

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -35,6 +35,9 @@ class Solution:
                 i += 1
         return results
 
-    '''
-    Needs improvement
-    '''
+'''
+*Needs improvement*
+Given an array nums of n integers, are there elements a, b, c in nums 
+such that a + b + c = 0? Find all unique triplets in the array which gives 
+the sum of zero. Note: The solution set must not contain duplicate triplets.
+'''
