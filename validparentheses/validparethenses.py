b0718fdb4a7ddd75ad47ca82ab2b562dfd53fcad
Carlos Alba <soradev4@gmail.com>
2019-06-13 22:27:49 -0700

    Update validparethenses.py

--- validparentheses/validparethenses.py
+++ validparentheses/validparethenses.py
@@ -11,3 +11,13 @@ class Solution:
                     return False
         # returns inverse
         return not stack
+    
+    '''
+    Runtime: 36 ms, faster than 86.22% of Python3 online submissions for Valid Parentheses.
+    Memory Usage: 13.3 MB, less than 18.20% of Python3 online submissions for Valid Parentheses.
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    An input string is valid if:
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Note that an empty string is also considered valid.
+    '''
