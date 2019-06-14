1a1a9b8cf049e1bcc1487c2a3c9bd913bd6d97c5
Carlos Alba <soradev4@gmail.com>
2019-06-13 22:36:46 -0700

    Update removeduplicatesfromsortedarray.py

--- removeduplicatesfromsortedarray/removeduplicatesfromsortedarray.py
+++ removeduplicatesfromsortedarray/removeduplicatesfromsortedarray.py
@@ -11,6 +11,8 @@ class Solution:
     '''
     Runtime: 64 ms, faster than 56.42% of Python3 online submissions for Remove Duplicates from Sorted Array.
     Memory Usage: 15 MB, less than 13.99% of Python3 online submissions for Remove Duplicates from Sorted Array.
+    Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
     '''
         
         
