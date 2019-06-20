54287001a01bf7da68eb05747220aa0607b14791
Carlos Alba <soradev4@gmail.com>
2019-06-20 12:08:01 -0700

    Create multiplystrings.py

---
+++ multiplystrings/multiplystrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # strings reperesent non-negative integers
+        # converting the strings to integers multiplying 
+        # the results of those functions I multiply them 
+        # and return the product of that operation
+            return str(int(num1) * int(num2))
+  '''
+  Runtime: 32 ms, faster than 99.02% of Python3 online submissions for Multiply Strings.
+  Memory Usage: 13.2 MB, less than 54.64% of Python3 online submissions for Multiply Strings.
+
+  '''
