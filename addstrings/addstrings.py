2ac5b0480423b9547e51ae0b935e584f2df2e4aa
Carlos Alba <soradev4@gmail.com>
2019-06-20 12:09:31 -0700

    Create addstrings.py

---
+++ addstrings/addstrings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        return str(int(num1) + int(num2))
+        
+'''
+Runtime: 44 ms, faster than 83.00% of Python3 online submissions for Add Strings.
+Memory Usage: 13.3 MB, less than 49.38% of Python3 online submissions for Add Strings.
+'''
